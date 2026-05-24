c689962241a4b4156d1a7dadd3cb38b57c697463
XARBE27 <dimitrikharbedia@gmail.com>
2026-05-24 21:32:54 +0400

    add

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ menu.addEventListener("click", () => {
     menu.innerHTML = "☰";
   }
 });
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.disabled = true;
+
+    setTimeout(() => {
+      button.disabled = false;
+    }, 1000);
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (!navLinks.contains(e.target) && !menu.contains(e.target)) {
+    navLinks.classList.remove("show");
+    menu.innerHTML = "☰";
+  }
+});
